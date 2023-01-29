bf352c7dfcbf05712a4ff6edec8e16dc66e4e432
ProElectro121 <94210870+ProElectro121@users.noreply.github.com>
2023-01-29 22:27:42 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1973,3 +1973,59 @@ class Solution{
         return scc;
     }
 };
+
+/*
+There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+basicalyy finding all the bridges in a graph
+*/
+
+
+class Solution {
+private:
+    int timer = 1;
+private:
+    void dfs(int node , int parent , vector<int>& vis , vector<int> adj[], int low[] ,int Tin[], vector<vector<int>>& bridges) {
+        vis[node] = 1;
+        low[node] = timer;
+        Tin[node] = timer;
+        timer++;
+
+        for(auto &it: adj[node]) {
+            if(it == parent) {
+                continue;
+            } 
+            if(!vis[it]) {
+                dfs(it , node ,vis ,adj , low , Tin , bridges);
+                low[node] = min(low[node] , low[it]);
+                if(low[it] > Tin[node]) {
+                    bridges.push_back({node , it});
+                }
+            }
+            else {
+                low[node] = min(low[node] , low[it]);
+            }
+        }
+    }
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> bridges;
+        vector<int> adj[n];
+
+        for(auto &i: connections) {
+            adj[i.front()].push_back(i.back());
+            adj[i.back()].push_back(i.front());
+        }
+        int low[n];
+        int Tin[n];
+
+        vector<int> vis(n , 0);
+
+        dfs(0 , -1 , vis , adj , low , Tin, bridges);
+        return bridges;
+    }
+};
